97356f78f29c015b8c05bfbde960851e461d868a
JasonRVCC <G00136378@W213-30.academic.raritanval.edu>
2015-10-16 13:06:16 -0400

    Version 1
    complete first sprite test

---
+++ LailaWalking/LailaWalking/LailaWalking/PlayerManager.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LailaWalking
+{
+    class PlayerManager
+    {
+        //might change this to be more useful
+        int animations = 4;
+
+        public Sprite playerSprite;
+        private float playerSpeed = 160.0f;
+        private Rectangle playerAreaLimit;
+
+        public long playerScore = 0;
+        public int livesRemaining = 3;
+        public bool destroyed = false;
+
+        //private Vector2 gunOffset = new Vector2(25, 10);
+        //private float shotTimer = 0.0f;
+        //private float minShotTimer = 0.2f;
+        private int playerRadius = 15;
+        //public ShotManager PlayerShotManager;
+
+        public PlayerManager(Texture2D texture, Rectangle initialFrame, int frameCount,
+            Rectangle screenBounds)
+        {
+            playerSprite = new Sprite(new Vector2(500, 500), texture, initialFrame, Vector2.Zero);
+
+            /*PlayerShotManager = new ShotManager(texture, new Rectangle(0, 300, 5, 5), 4, 2, 250f, 
+                screenBounds);*/
+
+            playerAreaLimit = new Rectangle(0, 0, screenBounds.Width, 
+                screenBounds.Height);
+
+            for (int x = 1; x < frameCount; x++)
+            {
+                playerSprite.AddFrame(0, new Rectangle(initialFrame.X + (initialFrame.Width * x),
+                    initialFrame.Y, initialFrame.Width, initialFrame.Height));
+            }
+
+            for (int y = 1; y < animations; y++)
+            {
+                playerSprite.AddAnimation();
+
+                for (int x = 0; x < frameCount; x++)
+                {
+                    playerSprite.AddFrame(y, new Rectangle(initialFrame.X + (initialFrame.Width * x),
+                    initialFrame.Y + (initialFrame.Height * y), 
+                    initialFrame.Width, initialFrame.Height));
+                }
+            }
+
+            playerSprite.CollisionRadius = playerRadius;
+        }
+
+        /*private void FireShot()
+        {
+            if (shotTimer >= minShotTimer)
+            {
+                PlayerShotManager.FireShot(playerSprite.Location + gunOffset, 
+                    new Vector2(0, -1), true);
+
+                shotTimer = 0.0f;
+            }
+        }*/
+
+        private void HandleKeyboardInput(KeyboardState keyState, GamePadState gamePadState)
+        {
+            switch (keyState.IsKeyDown(Keys.Up) || keyState.IsKeyDown(Keys.Down)
+                || keyState.IsKeyDown(Keys.Left) || keyState.IsKeyDown(Keys.Right))
+            {
+
+                case true:
+                    if (keyState.IsKeyDown(Keys.Up))
+                    {
+                        playerSprite.IsAnimating(true);
+                        playerSprite.ChangeAnimation(2);
+                        if (playerSprite.Frame == 0)
+                        {
+                            playerSprite.ChangeFrame(1);
+                        }
+                        playerSprite.Velocity += new Vector2(0, -1);
+                    }
+
+                    if (keyState.IsKeyDown(Keys.Down))
+                    {
+                        playerSprite.IsAnimating(true);
+                        playerSprite.ChangeAnimation(0);
+                        if (playerSprite.Frame == 0)
+                        {
+                            playerSprite.ChangeFrame(1);
+                        }
+                        playerSprite.Velocity += new Vector2(0, 1);
+                    }
+
+                    if (keyState.IsKeyDown(Keys.Left))
+                    {
+                        playerSprite.IsAnimating(true);
+                        playerSprite.ChangeAnimation(1);
+                        if (playerSprite.Frame == 0)
+                        {
+                            playerSprite.ChangeFrame(1);
+                        }
+                        playerSprite.Velocity += new Vector2(-1, 0);
+                    }
+
+
+                    if (keyState.IsKeyDown(Keys.Right))
+                    {
+                        playerSprite.IsAnimating(true);
+                        playerSprite.ChangeAnimation(3);
+                        if (playerSprite.Frame == 0)
+                        {
+                            playerSprite.ChangeFrame(1);
+                        }
+                        playerSprite.Velocity += new Vector2(1, 0);
+                    }
+
+                    break;
+
+
+                case false:
+                    if((gamePadState.ThumbSticks.Left.X == 0)&&
+                        (gamePadState.ThumbSticks.Left.Y == 0))
+                    {
+                        playerSprite.IsAnimating(false);
+                        playerSprite.ChangeFrame(0);
+                    }
+                    break;
+            }
+                
+            
+
+
+
+            /*if (keyState.IsKeyDown(Keys.Space))
+            {
+                FireShot();
+            }*/
+        }
+
+        private void HandleGamepadInput(GamePadState gamePadState, KeyboardState keyState)
+        {
+            Vector2 direction = new Vector2(gamePadState.ThumbSticks.Left.X,
+                -gamePadState.ThumbSticks.Left.Y);
+
+            if (direction.Y < 0)
+            {
+                playerSprite.IsAnimating(true);
+                playerSprite.ChangeAnimation(2);
+                if (playerSprite.Frame == 0)
+                {
+                    playerSprite.ChangeFrame(1);
+                }
+            }
+
+            if (direction.Y > 0)
+            {
+                playerSprite.IsAnimating(true);
+                playerSprite.ChangeAnimation(0);
+                if (playerSprite.Frame == 0)
+                {
+                    playerSprite.ChangeFrame(1);
+                }
+            }
+
+            if (direction.X < 0)
+            {
+                playerSprite.IsAnimating(true);
+                playerSprite.ChangeAnimation(1);
+                if (playerSprite.Frame == 0)
+                {
+                    playerSprite.ChangeFrame(1);
+                }
+            }
+
+            if (direction.X > 0)
+            {
+                playerSprite.IsAnimating(true);
+                playerSprite.ChangeAnimation(3);
+                if (playerSprite.Frame == 0)
+                {
+                    playerSprite.ChangeFrame(1);
+                }
+            }
+
+            if ((direction.X == 0) && (direction.Y == 0))
+            {
+                if (!keyState.IsKeyDown(Keys.Up) && !keyState.IsKeyDown(Keys.Down)
+                && !keyState.IsKeyDown(Keys.Left) && !keyState.IsKeyDown(Keys.Right))
+                {
+                    playerSprite.IsAnimating(false);
+                    playerSprite.ChangeFrame(0);
+                }
+            }
+
+            playerSprite.Velocity += direction;
+
+            /*if (gamePadState.Buttons.A == ButtonState.Pressed)
+            { FireShot(); }*/
+        }
+
+        private void ImposeMovementLimits()
+        {
+            Vector2 location = playerSprite.Location;
+
+            if(location.X < playerAreaLimit.X)
+            {location.X = playerAreaLimit.X;}
+
+            if(location.X > (playerAreaLimit.Right - playerSprite.Source.Width))
+            {location.X = (playerAreaLimit.Right - playerSprite.Source.Width);}
+
+            if(location.Y < playerAreaLimit.Y)
+            {location.Y = playerAreaLimit.Y;}
+
+            if(location.Y > (playerAreaLimit.Bottom - playerSprite.Source.Height))
+            {location.Y = (playerAreaLimit.Bottom - playerSprite.Source.Height);}
+
+            playerSprite.Location = location;
+        }
+
+        public void Update (GameTime gameTime)
+        {
+            //PlayerShotManager.Update(gameTime);
+
+            if(!destroyed)
+            {
+                playerSprite.Velocity = Vector2.Zero;
+
+                //shotTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                HandleKeyboardInput(Keyboard.GetState(), GamePad.GetState(PlayerIndex.One));
+                HandleGamepadInput(GamePad.GetState(PlayerIndex.One), Keyboard.GetState());
+
+                playerSprite.Velocity.Normalize();
+                playerSprite.Velocity *= playerSpeed;
+
+                playerSprite.Update(gameTime);
+                ImposeMovementLimits();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //PlayerShotManager.Draw(spriteBatch);
+
+            if(!destroyed)
+            {
+                playerSprite.Draw(spriteBatch);
+            }
+        }
+
+    }
+}
---
+++ LailaWalking/LailaWalking/LailaWalking/Sprite.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LailaWalking
+{
+    class Sprite
+    {
+        public Texture2D Texture;
+
+        //protected List<Rectangle> frames = new List<Rectangle>();
+        protected List<List<Rectangle>> frames = new List<List<Rectangle>>();
+        private int frameWidth = 0;
+        private int frameHeight = 0;
+        private int currentAnimation = 0;
+        private int currentFrame;
+        private float frameTime = 0.1f;
+        private float timeForCurrentFrame = 0.0f;
+        private bool animating = false;
+
+        private Color tintColor = Color.White;
+        private float rotation = 0.0f;
+
+        public int CollisionRadius = 0;
+        public int BoundingXPadding = 0;
+        public int BoundingYPadding = 0;
+
+        protected Vector2 location = Vector2.Zero;
+        protected Vector2 velocity = Vector2.Zero;
+
+        public Sprite(Vector2 location, Texture2D texture, Rectangle initialFrame, Vector2 velocity)
+        {
+            this.location = location;
+            Texture = texture;
+            this.velocity = velocity;
+
+            frames.Add(new List<Rectangle>());
+            frames[0].Add(initialFrame);
+            currentAnimation = 0;
+            frameWidth = initialFrame.Width;
+            frameHeight = initialFrame.Height;
+        }
+
+        public Vector2 Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        public Color TintColor
+        {
+            get { return tintColor; }
+            set { tintColor = value; }
+        }
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value % MathHelper.TwoPi; }
+        }
+
+        public int Frame
+        {
+            get { return currentFrame; }
+            set { currentFrame = (int)MathHelper.Clamp(value, 0, frames.Count - 1); }
+        }
+
+        public float FrameTime
+        {
+            get { return frameTime; }
+            set { frameTime = MathHelper.Max(0, value); }
+        }
+
+        public Rectangle Source
+        {
+            get { return frames[currentAnimation][currentFrame]; }
+        }
+
+        public Rectangle Destination
+        {
+            get { return new Rectangle((int)location.X, (int)location.Y, frameWidth, frameHeight); }
+        }
+
+        public Vector2 Center
+        {
+            get { return location + new Vector2(frameWidth / 2, frameHeight / 2); }
+        }
+
+        public Rectangle BoundingBoxRect
+        {
+            get
+            {
+                return new Rectangle((int)location.X + BoundingXPadding,
+                (int)location.Y + BoundingYPadding, frameWidth - (BoundingXPadding * 2),
+                frameHeight - (BoundingYPadding * 2));
+            }
+        }
+
+        public bool IsBoxColliding(Rectangle otherBox)
+        {
+            return BoundingBoxRect.Intersects(otherBox);
+        }
+
+        public bool IsCircleColliding(Vector2 otherCenter, float otherRadius)
+        {
+            if (Vector2.Distance(Center, otherCenter) < (CollisionRadius + otherRadius))
+                { return true; }
+            else
+                { return false; }
+        }
+
+        public void AddFrame(int animation, Rectangle frameRectangle)
+        {
+            frames[animation].Add(frameRectangle);
+        }
+
+        public void AddAnimation()
+        {
+            frames.Add(new List<Rectangle>());
+        }
+
+        public void ChangeAnimation(int animation)
+        {
+            currentAnimation = animation;
+        }
+
+        public void ChangeFrame(int frame)
+        {
+            currentFrame = frame;
+        }
+
+        //override just in case
+        public void ChangeFrame(int animation, int frame)
+        {
+            currentAnimation = animation;
+            currentFrame = frame;
+        }
+
+        public void IsAnimating(bool animating)
+        {
+            this.animating = animating;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            timeForCurrentFrame += elapsed;
+
+            if (animating)
+            {
+                if (timeForCurrentFrame >= 200)
+                {
+                    currentFrame = (currentFrame + 1) % (frames[currentAnimation].Count);
+                    timeForCurrentFrame = 0.0f;
+                }
+
+                location += (velocity * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Center, Source, tintColor, rotation,
+                new Vector2(frameWidth / 2, frameHeight / 2), 1.0f, SpriteEffects.None, 0.0f);
+        }
+    }
+}
